09f43dc31683bec8976af5e20e8e1adf5b7030e3
yuuki.ogura2 <Yuki_Ogura@sis2.saison.co.jp>
2024-01-30 16:19:51 +0900

    完了

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -1,12 +1,20 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+
 
 /**
  * Servlet implementation class IndexServlet
@@ -26,9 +34,44 @@ public class IndexServlet extends HttpServlet {
         /**
          * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
          */
-        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-                // TODO Auto-generated method stub
-                response.getWriter().append("Served at: ").append(request.getContextPath());
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        // 開くページ数を取得（デフォルトは1ページ目）
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+        // 最大件数と開始位置を指定してメッセージを取得
+        List<Task> task = em.createNamedQuery("getAllTask", Task.class)
+                                   .setFirstResult(15 * (page - 1))
+                                   .setMaxResults(15)
+                                   .getResultList();
+
+        // 全件数を取得
+        long task_count = (long)em.createNamedQuery("getTaskCount", Long.class)
+                                      .getSingleResult();
+
+        em.close();
+
+        request.setAttribute("task", task);
+        request.setAttribute("task_count", task_count);     // 全件数
+        request.setAttribute("page", page);                         // ページ数
+
+request.setAttribute("task", task);
+
+        request.setAttribute("task", task);
+
+        // フラッシュメッセージがセッションスコープにセットされていたら
+        // リクエストスコープに保存する（セッションスコープからは削除）
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
         }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/task/index.jsp");
+        rd.forward(request, response);
+    }
 
 }
